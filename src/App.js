b31ea706ca8c5676fa81ed870d3883c31d3b1993
quanjiahao <1134248919@qq.com>
2020-05-29 16:54:32 +0800

    测试

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Button,Row,Col} from 'antd'
 function App() {
   return (
     <div className="App">
-      <Button type="primary">测试11</Button>
+      <Button type="primary">测试1</Button>
       <Row>
       <Col xs={8}   md={24}   >
       Col
